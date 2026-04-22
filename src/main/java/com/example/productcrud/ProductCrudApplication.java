6751a64788615108d4bd84255d0ff5025a4e84ac
EvannnG <evanevanyehuda@gmail.com>
2026-04-22 09:10:11 +0700

    menambahkan fitur b

--- src/main/java/com/example/productcrud/ProductCrudApplication.java
+++ src/main/java/com/example/productcrud/ProductCrudApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductCrudApplication {
 // menambahkan fitur a
-
+// menambahkan fitur b
     public static void main(String[] args) {
         SpringApplication.run(ProductCrudApplication.class, args);
     }
